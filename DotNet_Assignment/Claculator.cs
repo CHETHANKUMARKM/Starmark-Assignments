fa721d8e3fe91656b10bd84016f0347831421cb7
Chethan Kumar K m <84614652+CHETHANKUMARKM@users.noreply.github.com>
2023-01-06 17:14:49 +0530

    Add files via upload

---
+++ DotNet_Assignment/Claculator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConApp
+{
+    class Claculator
+    {
+        static void Main()
+        {
+            string condition = "";
+             while(true)
+            {
+                if(condition == "" || condition == "yes") { 
+                Console.WriteLine("Enter the first number");
+                int a = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("Enter the second number");
+                int b = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("Enter symbol: +,-,*,/");
+                int res;
+                string operation = Console.ReadLine();
+                switch (operation)
+                {
+                    case "+":
+                        res = a + b;
+                        Console.WriteLine("Result : " + res);
+                        break;
+                    case "-":
+                        res = a - b;
+                        Console.WriteLine("Result : " + res);
+                        break;
+                    case "*":
+                        res = a * b;
+                        Console.WriteLine("Result : " + res);
+                        break;
+                    case "/":
+                        if (b == 0)
+                        {
+                            Console.WriteLine("Divide by zero error");
+                            break;
+                        }
+                        else
+                        {
+                            res = a / b;
+                            Console.WriteLine("Result : " + res);
+                            break;
+                        }
+
+                    case "default":
+                        Console.WriteLine("Invalid Input");
+                        break;
+                }
+                    Console.WriteLine("want to Continuee yes or no");
+                   condition = Console.ReadLine();
+                    
+                }
+            }
+        }
+
+
+        
+    }
+}
---
+++ DotNet_Assignment/DisplayOddandEven.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConApp
+{
+    class DisplayOddandEven
+    {
+        static void Main(string[] args)
+        {
+             void Check()
+            {
+                Console.WriteLine("Enter the total number ");
+                int total = Convert.ToInt32(Console.ReadLine()) ;
+
+                int[] numbers = new int[total];
+                Console.WriteLine("Enter the "+total+" number ");
+                for (int i=0;i<numbers.Length;i++)
+                {
+                    numbers[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                //Console.WriteLine( "numbers");
+
+                for(int j=0;j<numbers.Length;j++)
+                {
+                    if(numbers[j]%2==0)
+                    {
+                        Console.WriteLine("The "+numbers[j]+"is EvenNumber");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The " + numbers[j] + "is oddNumber");
+                    }
+                    
+                }
+          
+            }
+            Check();
+           
+        }
+    }
+}
